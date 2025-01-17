f5253bfe72b5c6809f56c65b501480152efb8a86
nashrulmalik <nashrulmalik@gmail.com>
2025-01-17 21:41:36 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Red Hat, sans-serif', // Adds a new `font-display` class
+      }
+    }
   },
   plugins: [],
 }
